ba0af631afc8bda8a7e1678205e687f6caff6192
فدوى ناصف ميلاد ناصف <naseffadwa@gmail.com>
2025-10-15 14:35:03 -0700

    Update LocationTester.java

--- project1_location/src/LocationTester.java
+++ project1_location/src/LocationTester.java
@@ -1,13 +1,13 @@
 public class LocationTester {
     public static void main(String[] args) {
-        // موقع المستخدم الحالي (مثال: تاجوراء)
+        // موقع المستخدم الحالي ( تاجوراء)
         SimpleLocation myLocation = new SimpleLocation(32.9036, 13.2265);
 
         // موقع كلية تقنية المعلومات في طرابلس
         SimpleLocation collegeLocation = new SimpleLocation(32.8872, 13.1913);
 
         // طباعة المواقع
-        System.out.print("موقعي الحالي: ");
+        System.out.print("موقعي الحالي:");
         myLocation.printLocation();
         System.out.print("موقع الكلية: ");
         collegeLocation.printLocation();
